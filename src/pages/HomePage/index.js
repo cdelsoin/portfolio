e1211d3b2ad90a1cc88a17dd132bbc89d55c5ee4
Cliff Delsoin <cdelsoin@gmail.com>
2020-03-29 17:06:10 -0400

    make HomePage responsive

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -8,9 +8,13 @@ export function HomePage() {
         <div css={styles.container}>
             <div css={styles.bio}>
                 <img src={logo} css={styles.headshot} alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
+                <div>
+                    <p css={styles.bioText}>
+                        I'm Cliff Delsoin, a software engineer based out of Boston, MA specializing in React and React
+                        Native application development. If you'd like to get in touch feel free to check out my{' '}
+                        <a href="https://www.linkedin.com/in/cliffdelsoin/">LinkedIn</a>
+                    </p>
+                </div>
             </div>
         </div>
     );
--- src/pages/HomePage/styles.js
+++ src/pages/HomePage/styles.js
@@ -1,24 +1,54 @@
 const styles = {
     container: {
-        backgroundColor: 'teal',
-        minHeight: '100vh',
+        backgroundColor: '#262626',
+        height: '100vh',
+        width: '100%',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     headshot: {
-        height: '35vmin',
+        height: '30vmin',
+        maxHeight: '320px',
         borderRadius: '50%',
         border: '4px solid white',
         padding: '5px',
+        '@media(max-width: 820px)': {
+            height: '50vmin'
+        },
+        '@media(max-width: 420px)': {
+            height: '60vmin'
+        }
     },
     bio: {
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'center',
+        justifyContent: 'space-around',
         alignItems: 'center',
         color: 'white',
+        width: '900px',
+        '@media(max-width: 820px)': {
+            flexDirection: 'column',
+            justifyContent: 'center'
+        },
+        '@media(max-width: 420px)': {
+            flexDirection: 'column',
+            justifyContent: 'center',
+            width: '80%',
+            fontSize: '5vmin'
+        }
     },
+    bioText: {
+        width: '500px',
+        fontSize: '24px',
+        '@media(max-width: 820px)': {
+            fontSize: '4vmin'
+        },
+        '@media(max-width: 420px)': {
+            width: '100%',
+            fontSize: '5vmin'
+        }
+    }
 };
 
 export default styles;
