a5ac72cbc8da8860c526b7d26f00b9158b3e1805
Cliff Delsoin <cdelsoin@gmail.com>
2020-04-07 02:18:21 -0400

    Updates

---
+++ precache-manifest.43eddd0ecc224e474c32c4ed4947b8e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e3c61fff50bddd7c91d95c00c3b536",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "698e24c6802e311d0322",
+    "url": "/portfolio/static/css/main.ab891f25.chunk.css"
+  },
+  {
+    "revision": "17027f64b469f3b20f9c",
+    "url": "/portfolio/static/js/2.b7ad8f03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b7ad8f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698e24c6802e311d0322",
+    "url": "/portfolio/static/js/main.813ce02e.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "632788c60d435284304a983d6cb86e41",
+    "url": "/portfolio/static/media/headshot.632788c6.jpg"
+  }
+]);
